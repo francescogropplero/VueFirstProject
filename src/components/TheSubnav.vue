a68e344ac5afea760c1ea0531bdf39879dc04143
Francesco Gropplero <fra.gro.tro@gmail.com>
2023-04-15 11:26:26 +0200

    test subnav

--- src/components/TheSubnav.vue
+++ src/components/TheSubnav.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="h-16 w-full border-b border-solid border-brand-panna bg-white">
-    <div class="flex h-full items-center px-8"><div>
+    <div class="flex h-full items-center px-8">
+      <div v-if="onJobResultsPage">
         <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
-        <span><span class="text-brand-verde font-bold">1653</span> jobs matched</span>
-    </div></div>
+        <span
+          ><span class="font-bold text-brand-verde">1653</span> jobs
+          matched</span
+        >
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "TheSubnav",
+  data() {
+    return {
+      onJobResultsPage: true,
+    };
+  },
 };
 </script>
 

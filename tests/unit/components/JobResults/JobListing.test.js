30ea1fddebe939ea5a4b114ddc25b0a117886956
Francesco Gropplero <fra.gro.tro@gmail.com>
2023-04-22 14:28:40 +0200

    succesfully tested mock backend

--- tests/unit/components/JobResults/JobListing.test.js
+++ tests/unit/components/JobResults/JobListing.test.js
@@ -4,7 +4,12 @@ import { RouterLinkStub } from "@vue/test-utils";
 import JobListing from "@/components/JobResults/JobListing.vue";
 
 describe("JobListing", () => {
-  it("renders job title", () => {
+  const createJobProps = (jobProps = {}) => ({
+    title: "Vue Developer",
+    organization: "AirBnB",
+    ...jobProps,
+  });
+  const renderJobListing = (jobProps) => {
     render(JobListing, {
       global: {
         stubs: {
@@ -13,26 +18,40 @@ describe("JobListing", () => {
       },
       props: {
         job: {
-          title: "Vue Developer",
+          ...jobProps,
         },
       },
     });
-    expect(screen.getByText("Vue Developer")).toBeInTheDocument();
-  });
-  it("renders job organization", () => {
-    render(JobListing, {
-        global: {
-          stubs: {
-            "router-link": RouterLinkStub,
-          },
-        },
-        props: {
-          job: {
-            organization: "AirBnB",
-          },
-        },
-      });
-      expect(screen.getByText("AirBnB")).toBeInTheDocument();
-    });
+  };
+
+  it("renders job title", () => {
+    const jobProps = createJobProps({ title: "React Developer"})
+    renderJobListing(jobProps);
+    expect(screen.getByText("React Developer")).toBeInTheDocument();
   });
 
+  it("renders job organization", () => {
+    const jobProps = createJobProps({ organization: "Apple"})
+    renderJobListing(jobProps);
+    expect(screen.getByText("Apple")).toBeInTheDocument();
+  });
+
+  it("renders job locations", () => {
+    const jobProps = createJobProps({
+      locations: ["Orlando", "Jacksonville"],
+    });
+    renderJobListing(jobProps);
+    expect(screen.getByText("Orlando")).toBeInTheDocument();
+    expect(screen.getByText("Jacksonville")).toBeInTheDocument();
+  })
+
+  it("renders job qualifications", () => {
+    const jobProps = createJobProps({
+      minimumQualifications: ["Code", "Develop"],
+    });
+    renderJobListing(jobProps);
+    expect(screen.getByText("Code")).toBeInTheDocument();
+    expect(screen.getByText("Develop")).toBeInTheDocument();
+
+  })
+});
